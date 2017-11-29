e54ae0fd4847dcf92e48d5d272fac3fcfc37b871
mishroom <mishall.afzal@gmail.com>
2017-11-29 15:17:47 -0800

    Completed "functional pattern/stack"

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,29 +3,19 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var counter = 0;
-  var result; 
+ 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    storage[counter] = value;
-    counter ++;
+   
   };
 
   someInstance.dequeue = function() {
-    if (counter) {
-      counter --;
-    }
-    result = storage[0];
-    for (var i = 0; i <= counter; i++) {
-      storage[i] = storage[i + 1];
-    }
-    delete storage [counter];
-    return result;
+
   };
 
   someInstance.size = function() {
-    return counter;
+  
   };
 
   return someInstance;
