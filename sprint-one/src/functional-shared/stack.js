aeb84e35763e472b89ff6febcfa3b33ffeb2967c
mishroom <mishall.afzal@gmail.com>
2017-11-29 16:47:13 -0800

    half done stack functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
-var Stack = function() {
+var Stack = function(loc) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var result = {};
+
+  result['count'] = 0;  
+  result['storage'] = {};
+  
+  extend(result, stackMethods);
+  return result;
 };
 
-var stackMethods = {};
+function extend(obj, method) {
+  for (var key in method) {
+    obj[key] = method[key];  
+  }
+}
 
+
+var stackMethods = {
+  pop : function() {},
+  push : function(value) {},
+  size : function() {
+    return 0;
+  }
+};
 
