aca042e9d208dfc4f17f59e932a49e54122ed853
mishroom <mishall.afzal@gmail.com>
2017-11-30 11:46:56 -0800

    queue psuedoclassical half

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
+  this.popped;
+};
+
+// Queue.prototype = {
+//   // enqueued: function (value) {},
+//   // dequeued: function () {},
+//   //size: function () {return this.count;}
+// };
+Queue.prototype.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+Queue.prototype.pop = function() {
+  if (this.count) {
+    this.count--;  
+  }
+  this.popped = this.storage['0'];
+  delete 
+};
+
+Queue.prototype.size = function() {
+  return this.count;
 };
 
 
