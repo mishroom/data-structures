3bf885c911076e221dbc5c8a76a8ab53df2dc980
mishroom <mishall.afzal@gmail.com>
2017-11-30 17:40:23 -0800

    SOME THINGS ARE WORKING SOME ARE NOT IDK MAN

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -19,18 +19,34 @@ var LinkedList = function() {
     //if the tail does not exist, we want to set the tail = new node
     //if tail does exists, we need rename it to its value, change next
     // value, we need to create the new tail
-
+    if (!list.head) {
+      list.head = Node(value);
+      list.head.next = list.head.value;
+    }
     if (list.tail) {
       var name = list.tail.value;
       list[name] = list.tail;
       list[name].next = value;
     }
     list.tail = Node(value);
+    //}
+    
   };
 
   list.removeHead = function() {
-    //O(1)    
-
+    //O(1) 
+    // create var to keep track of head.next   
+    //delete head
+    //reassigne head.next to head
+    if (list.head) {
+      var nextNode = list.head.next;
+      delete list.head;
+      
+    }
+    list.head = list[nextNode];
+      return nextNode;
+  
+    
   };
 
   list.contains = function(target) {
@@ -53,15 +69,25 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
 // list = {
 //   head: {value: val, next: yt6},
 //   yt6: {value: yt6, next: hello},
 //   hello: {value: hello, next: blubber}
 //   blubber: {value: blubber, next: cookies}
-  
-// }
+//   tail: {value: cookies, next:null} 
+// }  
 
-// node = {value: cookies, next:null}
+
+// list = {
+  // 
+//   head: {value: yt6, next: hello},
+//   hello: {value: hello, next: blubber}
+//   blubber: {value: blubber, next: cookies}
+//   tail: {value: cookies, next:null} 
+// }      
+
+// node = 
 
    //O(n)
     //iterate through for (key in list), 
