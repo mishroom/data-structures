4cb6aebf98edd14d8140578d1d41ea4b89cf7f52
mishroom <mishall.afzal@gmail.com>
2017-12-01 19:01:43 -0800

    hash table - completed remove

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -11,14 +11,14 @@ HashTable.prototype.insert = function(k, v) {
   //   this._storage.set(index, arr);
   // }
   this._storage.set(index, arr); 
-  console.log('bucket in insert:', this._storage.get(index));
+  // console.log('bucket in insert:', this._storage.get(index));
   // [[[a, b]]]
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  console.log('bucket in retrieve:', bucket);
+  // console.log('bucket in retrieve:', bucket);
   for (var i = 0; i < bucket.length; i++) {
     if (bucket[i][0] === k) {
       return bucket[i][1];
@@ -29,7 +29,17 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
+  // console.log('storage:', this._storage.get(index), 'index:', index, 'k:', k);
+  var result;
+  // console.log(this._storage.get(index)[0][0]);
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      console.log(k);
+      result = this._storage.get(index)[i][1];
+      this._storage.get(index).splice(i, 1);
+    }
+    return result;
+  }
 };
 
 
