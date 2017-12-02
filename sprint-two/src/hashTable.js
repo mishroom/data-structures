73204aadaa1c4eeab8ff076a2cd49e717b513b0b
mishroom <mishall.afzal@gmail.com>
2017-12-01 20:17:40 -0800

    completed hash

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,22 +5,33 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var arr = [];
-  arr.push([k, v]);
-  // if (!this._storage.get) {
-  //   this._storage.set(index, arr);
-  // }
-  this._storage.set(index, arr); 
-  // console.log('bucket in insert:', this._storage.get(index));
+  //console.log("index: ", index);
+  // this means there is nothing in the bucket index
+  if (!this._storage.get(index)) {
+    this._storage.set(index, []);
+  } 
+  // if the bucket index doesn't have an overall bucket, need
+  // to add that bucket in
+  // if there is, just set the new tuplet pairs in it
+  if (this._storage.get(index).length) {
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        this._storage.get(index).splice(i, 1);
+      }
+    }
+  }
+  this._storage.get(index).push([k, v]); 
   // [[[a, b]]]
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  // console.log('bucket in retrieve:', bucket);
+  //console.log('bucket in retrieve:', bucket);
   for (var i = 0; i < bucket.length; i++) {
     if (bucket[i][0] === k) {
+      //console.log("bucket: ", bucket, "pair: ", bucket[i]);
       return bucket[i][1];
     }
   }
@@ -34,7 +45,7 @@ HashTable.prototype.remove = function(k) {
   // console.log(this._storage.get(index)[0][0]);
   for (var i = 0; i < this._storage.get(index).length; i++) {
     if (this._storage.get(index)[i][0] === k) {
-      console.log(k);
+      //console.log(k);
       result = this._storage.get(index)[i][1];
       this._storage.get(index).splice(i, 1);
     }
