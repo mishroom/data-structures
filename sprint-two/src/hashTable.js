3c2fc1a2314d1810407b4554c9c8513df7f683e5
mishroom <mishall.afzal@gmail.com>
2017-12-01 16:34:15 -0800

    half hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,10 +5,26 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var arr = [];
+  arr.push([k ,v]);
+  // if (!this._storage.get) {
+  //   this._storage.set(index, arr);
+  // }
+  this._storage.set(index, arr); 
+  console.log('bucket in insert:', this._storage.get(index));
+  // [[[a, b]]]
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  console.log('bucket in retrieve:', bucket);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
+  
 };
 
 HashTable.prototype.remove = function(k) {
